f03176b8f82d5229e52c8340a37ddd88d661a15d
Oleg Kariuk <olegkariuk@gmail.com>
2015-01-27 17:43:33 +0200

    Add Google cloud storage for images

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
 
   has_attached_file :image, 
-    path: ":rails_root/public/:attachment/:id/:basename_:style.:extension",
+    path: ":attachment/:id/:basename_:style.:extension",
     url: "/:attachment/:id/:basename_:style.:extension",
     styles: {
       thumb:   ['100x100#',  :jpg, :quality => 70],
       preview: ['200x200#',  :jpg, :quality => 70],
       large:   ['600>',      :jpg, :quality => 70],
       retina:  ['1200>',     :jpg, :quality => 30]
-    }
+    },
+    storage: :fog,
+    fog_credentials: "#{Rails.root}/config/gce.yml",
+    fog_directory: "autopark"
 
   validates :title, :description, presence: true
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
